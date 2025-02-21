c704526d86b6f8471d5cef3908b7d0aa39e320c4
Isaiah Ellis <isaiahellis1234@gmail.com>
2025-02-20 18:57:26 -0800

    First
    First

---
+++ JS/script.js
@@ -0,0 +1,62 @@
+const canvas = document.getElementById("particleCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particlesArray = [];
+const numParticles = 50;
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 5 + 2;
+    this.speedX = (Math.random() - 0.5) * 0.5;
+    this.speedY = (Math.random() - 0.5) * 0.5;
+    this.hue = Math.random() * 360; // Start with a random color (HSL format)
+    this.hueSpeed = Math.random() * 0.5 + 0.2; // Speed of color change
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    if (this.x > canvas.width || this.x < 0) this.speedX *= -1;
+    if (this.y > canvas.height || this.y < 0) this.speedY *= -1;
+
+    this.hue += this.hueSpeed; // Gradually change color
+    if (this.hue > 360) this.hue = 0; // Loop hue value
+  }
+
+  draw() {
+    ctx.fillStyle = `hsl(${this.hue}, 100%, 60%)`; // Use HSL for smooth transitions
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+}
+
+function initParticles() {
+  for (let i = 0; i < numParticles; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+function animateParticles() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particlesArray.forEach((particle) => {
+    particle.update();
+    particle.draw();
+  });
+  requestAnimationFrame(animateParticles);
+}
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+initParticles();
+animateParticles();
